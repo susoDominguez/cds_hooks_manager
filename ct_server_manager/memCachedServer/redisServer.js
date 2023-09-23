38f96fcd53ba7210211d15c1144da58fe42ac94c
Jesus Dominguez <jesus.dominguez@kcl.ac.uk>
2023-09-23 21:50:01 +0100

    Merge branch 'main' of https://github.com/susoDominguez/cds_hooks_manager

--- ct_server_manager/memCachedServer/redisServer.js
+++ ct_server_manager/memCachedServer/redisServer.js
@@ -7,17 +7,20 @@ const { REDIS_HOST, REDIS_PORT } = process.env;
 let redisClient;
 
 (async () => {
+  redisClient = createClient({ url: `redis://${REDIS_HOST || '127.0.0.1'}:${REDIS_PORT || '6379'}` });
+/*
+(async () => {
   redisClient = createClient({
     host: REDIS_HOST || '127.0.0.1'
     ,port: REDIS_PORT || '6379'
    // ,detect_buffers: true
 });
-
+*/
   redisClient.on("ready", () => logger.debug('redis server is ready'))
    
   redisClient.on("connect", () => logger.debug('connect redis: success'))
 
-  redisClient.on("error", (error) => logger.error(`Error : ${error}`));
+  redisClient.on("error", (err) => logger.error(`${err}`) );
 
   await redisClient.connect();
 })();
